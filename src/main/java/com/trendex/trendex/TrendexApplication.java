be2e508dcf2f0fa6494ff86ddd8ca94d457bfc1e
geonwoo0215 <gw980215@naver.com>
2024-06-21 06:00:59 +0900

    initial commit

---
+++ src/main/java/com/trendex/trendex/TrendexApplication.java
@@ -0,0 +1,13 @@
+package com.trendex.trendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trendex/trendex/TrendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendex.trendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
